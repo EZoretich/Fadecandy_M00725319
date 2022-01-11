8a110cba74746c3a5a021cd1d25782255cae6dfa
EZoretich <ez111@live.mdx.ac.uk>
2022-01-11 19:41:41 +0000

    Update Choose_Favourite_Color.py
    Whenever you insert a number from the list, it now display the whole led wall of that said color.

--- examples/Choose_Favourite_Color.py
+++ examples/Choose_Favourite_Color.py
@@ -5,7 +5,7 @@ import opc
 import time
 import colorsys
 
-led_wall = [(0,0,0)]*360
+led_wall = [(0, 0, 0)]*360
 client = opc.Client('localhost:7890')
 client.put_pixels(led_wall)
 client.put_pixels(led_wall)
@@ -29,9 +29,43 @@ while True:
     else:
         choice = input("Not a number! Please insert the number corresponding to you favourite color: ")
 
-if choice == 1:
-    for led in range(360):
-        led_wall[led] = (0,255,0)
-        time.sleep(0.1)
-        client.put_pixels(led_wall)
-        client.put_pixels(led_wall)
+if choice == 1: # GREEN
+    led_wall = [(0, 255, 0)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+
+if choice == 2: # PURPLE
+    led_wall = [(127, 0 , 255)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+        
+if choice == 3: # BLUE
+    led_wall = [(0, 0, 255)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
+if choice == 4: # GREY
+    led_wall = [(150, 150, 150)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
+if choice == 5: # RED
+    led_wall = [(255, 0, 0)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
+if choice == 6: # YELLOW
+    led_wall = [(255, 255, 0)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
+if choice == 7: # ORANGE
+    led_wall = [(255, 128, 0)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
+if choice == 8: # PINK
+    led_wall = [(255, 102, 178)]*360
+    time.sleep(.1)
+    client.put_pixels(led_wall)
+    
