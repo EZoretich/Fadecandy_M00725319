97cf09444a826115ebd7e458028162128ac5fb4a
EZoretich <ez111@live.mdx.ac.uk>
2022-01-14 20:34:51 +0000

    Work on Sky animation
    Starry Night is now generating x number of stars in random positions. It changes every time the code runs

--- examples/Starry Night.py
+++ examples/Starry Night.py
@@ -5,15 +5,50 @@ import opc
 import time
 import colorsys
 import random
-from random import choice
 
 led_wall = [(0,0,255)]*360 #LED wall will display only blue
-
+random_stars = [(0,0,255)]*360
 client = opc.Client('localhost:7890')
-client.put_pixels(led_wall)
-client.put_pixels(led_wall)
 
+fade = 10
+
+
+def Stars(random_stars):
+    for stars in range(50):
+        rows = random.randrange(0,5)
+        led = random.randrange(0,59)
+        #print(f'random star will be at y %s and x %s', y, x)
+        #random_stars.append((y,x))
+        random_stars[led + rows*60] = (255,255,255)
+    return random_stars
+        
+#print(random_stars)
+
+time.sleep(0.1)
+
+time.sleep(0.1)
+client.put_pixels(Stars(led_wall))
+random_stars=(Stars(led_wall))
+z=255
 while True:
-    random.choices(led_wall, 15) = (255,255,255)
-    time.sleep(.1)
-    client.put_pixels(leds)
+    
+    for led in enumerate(random_stars):
+        r,g,b = led[1]
+        r = r + fade
+        g = g + fade
+        b = b + fade
+
+        fade_star = (r,g,b)
+        #print("led 0")
+        print(random_stars[led[0]])
+        #random_stars[led] = fade_star[0]
+        
+        time.sleep(.5)
+        if random_stars[led[0]] == z and random_stars[led[1]] == z and random_stars[led[2]] == z :
+            z=z+fade
+            fade = -fade
+            random_stars[led[0]] = fade_star
+            print(hit)
+            
+    time.sleep(0.1)
+    #client.put_pixels(Stars(random_stars))
--- examples/Super Mario.py
+++ examples/Super Mario.py
@@ -2,17 +2,17 @@ import opc
 import time
 import random
 
-def  marioDude():     
+def  Mario():     
     led = 0
 
-    while led<60:
+    while led < 60:
         for rows in range(6):
         #red              head                             jumper
-            if ( led > 10 and led < 14 and rows == 2) or ( rows == 4 and led == 12):
+            if ( led > 12 and led < 16 and rows == 2) or ( rows == 4 and (led == 13 or led == 15)  ) :
                 leds[led + rows*60] = (255,0,0)
-            if ( led > 10 and led < 15 and rows == 3):
+            if ( led > 12 and led < 17 and rows == 3):
                 leds[led + rows*60] = (255,228,225)
-            if ( led > 10 and led < 14 and rows == 5) or ( rows == 4 and (led == 11 or led == 13)  ) :
+            if ( led > 12 and led < 16 and rows == 5) or ( rows == 4 and led == 14):
                 leds[led + rows*60] = (0,0,255)
            #if
            #leds[led + rows*60] = (0,70,255)
@@ -21,10 +21,17 @@ def  marioDude():
         led = led + 1
     return leds
 
+def Tube():
+    led = 0
 
-
-
-
+    while led < 60:
+        for rows in range(6):
+            if ( led > 30 and led < 35 and rows == 3) or ( rows == 4 and ( led == 32 or led == 33) ) or ( rows == 5 and ( led == 32 or led == 33 ) ):
+                leds[led + rows*60] = (50,205,50)
+            if (led == 34 and (rows == 4 or rows == 5)) or (rows == 3 and led == 35):
+                leds[led + rows*60] = (34,139,34)
+        led = led + 1
+        return leds
 
 leds = [(255,255,255)]*360 #white
 
@@ -32,15 +39,15 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 
 led = 0
-while led<60: #scroll all rows at the same time
+while led < 60: #scroll all rows at the same time
     for rows in range(6):
         leds[led + rows*60] = (110,255,255)
 
 
     led = led + 1
     client.put_pixels(leds)
-    
-    leds=marioDude()
+
+    leds = Mario()
     client.put_pixels(leds)
     time.sleep(.1)
 
