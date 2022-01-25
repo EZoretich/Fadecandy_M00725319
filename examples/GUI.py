90f482b9158f0589bdd9bb2937db1ed2a4f212b5
EZoretich <ez111@live.mdx.ac.uk>
2022-01-25 14:49:15 +0000

    GUI Demo
    Learning about Python built it GUI

---
+++ examples/GUI.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk() # object that is root window
+window.title('TKInter Demo')
+
+text = tk.Label(text = 'Hello World!')
+#text.pack()
+
+window.mainloop() # event loop --> how it starts
