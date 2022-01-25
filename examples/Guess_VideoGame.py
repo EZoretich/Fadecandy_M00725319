6eedc718cf07ddeee8293cbd8747f77709efeff1
EZoretich <ez111@live.mdx.ac.uk>
2022-01-25 22:47:05 +0000

    Update Guess_VideoGame.py
    More function created

--- examples/Guess_VideoGame.py
+++ examples/Guess_VideoGame.py
@@ -90,6 +90,7 @@ def Mr_Pacman():
     led = 0
     while led < 60:
         for rows in range(6):
+                        # Yellow LEDs (Pacman's Body)
             if (( led > 20 and led < 24 and rows == 0) or
                 (led > 17 and led < 27 and rows == 1) or
                 (led > 16 and led < 28 and rows == 2) or
@@ -97,6 +98,7 @@ def Mr_Pacman():
                 (led > 17 and led < 27 and rows == 4) or
                 (led > 20 and led < 24 and rows == 5)): 
                 led_wall[led + rows*60] = (255,255,0)
+                        # White LEDs (Dots)
             if ((led == 30 or led == 33 or led == 36) and rows == 3):
                 led_wall[led + rows*60] = (255,255,255)
         led = led + 1
@@ -107,11 +109,13 @@ def Blue_Ghost():
     led = 0
     while led < 60:
         for rows in range(6):
+                        # Blue LEDs (Ghost's Body)
             if ((led > 4 and led < 8 and rows == 1) or
                 (led > 2 and led < 10 and (rows == 3 or rows == 4)) or
                 ((led > 2 and led < 10) and rows == 2) or
                 (led == 3 or led == 5 or led == 7 or led == 9) and rows == 5):
                  led_wall[led + rows*60] = (51,153,255)
+                         # White LEDs (Ghost's Eyes)
             if ((led == 5 or led == 8) and rows == 2):
                 led_wall[led + rows*60] = (255,255,255)
         led = led + 1
@@ -122,11 +126,13 @@ def Purple_Ghost():
     led = 0
     while led < 60:
         for rows in range(6):
+                        # Purple LEDs (Ghost's Body)
             if ((led > 52  and led < 56 and rows == 1) or
                 (led > 50 and led < 58 and (rows == 3 or rows == 4)) or
                 ((led > 50 and led < 58 ) and rows == 2) or
                 (led == 51 or led == 53 or led == 55 or led == 57) and rows == 5):
                  led_wall[led + rows*60] = (121,54,189)
+                         # White LEDs (Ghost's Eyes)
             if ((led == 52 or led == 55) and rows == 2):
                 led_wall[led + rows*60] = (255,255,255)
         led = led + 1
@@ -137,9 +143,11 @@ def Cherry():
     led = 0
     while led < 60:
         for rows in range(6):
+                        # Red LEDs (Cherry)
             if (((led > 39 and led < 44) and rows == 3) or
                 ((led == 41 or led == 42) and (rows == 2 or rows == 4))):
                 led_wall[led + rows*60] = (255,0,0)
+                        # Green LEDs (Cherry's Stem)
             if ((led == 43 and rows == 0) or
                 (led == 42 and rows == 1)):
                 led_wall[led + rows*60] = (0,255,0)
@@ -156,9 +164,74 @@ def Pacman():
 # -------------------------------------------------------- POKEMON DISPLAY
 # --------------- PokeBall Function
 def Pokeball():
+    led = 0
+    while led < 60:
+        for rows in range(6):
+                        # Red LEDs (Pokeball TOP)
+            if (((led > 9 and led < 17) and rows == 1) or
+                ((led > 8 and led < 18) and rows == 2)):
+                led_wall[led + rows*60] = (255,0,0)
+                        # White LEDs (Pokeball BOTTOM)
+            if (((led > 9 and led < 17) and rows == 5) or
+                ((led > 8 and led < 18) and rows == 4) or
+                (led == 13 and rows == 3)):
+                led_wall[led + rows*60] = (255,255,255)
+                        # Dark Gray (Pokeball's line)
+            if ((( led > 11 and led < 15) and (rows == 2 or rows == 4)) or
+                 ((led > 8 and led < 13 or led > 13 and led < 18) and rows == 3)):
+                led_wall[led + rows*60] = (64,64,64)
+        led = led + 1
+    return led_wall
+
+# --------------- Pikachu Function
+def Pikachu():
+    led = 0
+    while led < 60:
+        for rows in range(6):
+                        # Yellow (Pikachu's Body)
+            if (((led > 23 and led < 31) and
+                 (rows == 2 or rows == 3 or rows == 4 or rows == 5)) or
+                ((led == 25 or led == 29) and rows == 1)):
+                led_wall[led + rows*60] = (255,255,0)
+            if (led == 31 and rows == 4):
+                led_wall[led + rows*60] = (204,153,51)
+            if (led == 32 and rows == 3):
+                led_wall[led + rows*60] = (85,58,4)
+            if ((led == 24 or led == 30) and rows == 3):
+                led_wall[led + rows*60] = (255,0,0)
+        led = led + 1
+    return led_wall
     
 
+# --------------- Charmander Function
+def Charmander():
+    led = 0
+    while led < 60:
+        for rows in range(6):
+                        # Orange LEDs (Charmander's Body)
+            if (((led > 39 and led < 44) and (rows == 0 or rows == 3)) or
+                ((led > 38 and led < 45) and (rows == 1 or rows == 2)) or
+                ((led  > 38 and led < 45) and rows == 4) or
+                ((led > 43 and led < 46 or led == 39) and rows == 5) or
+                (led == 46 and rows == 4)):
+                led_wall[led + rows*60] = (255,128,0)
+                        # Yellow LEDs (Charmander's Belly & Flame)
+            if (((led > 40 and led < 43) and rows == 4) or
+                ((led > 39 and led < 44) and rows == 5) or
+                (led == 47 and rows == 3)):
+                led_wall[led + rows*60] = (255,255,0)
+                        # Red LEDs (Charmander's Flame)
+            if (((led > 46 and led < 49) and rows == 2) or
+                (led == 48 and rows == 3)):
+                led_wall[led + rows*60] = (255,0,0)
+        led = led + 1
+    return led_wall
 
+#---------------------------------------------------- FULL POKEMON FUNCTION
+def Pokemon():
+    client.put_pixels(Charmander())
+    client.put_pixels(Pokeball())
+    client.put_pixels(Pikachu())
 #------------------------------------------------------------------------------------------
 led_wall = [(0,0,0)]*360 #black
 
@@ -167,4 +240,5 @@ client.put_pixels(led_wall)
 
 #Pacman()
 #Super_Mario()
+#Pokemon()
 time.sleep(.1)
